e6b56470ec1544b0ddcc0b42cb743251c49cbf3f
Jeff Chang <pokejeffcrater@gmail.com>
2023-04-05 14:50:14 -0500

    adding original repeat file

---
+++ queue.sh
@@ -0,0 +1,33 @@
+#!/home/hertin/softwares/zsh/bin/zsh
+# #!/bin/bash
+repeat=$1
+pid=$2
+script=$3
+USERNAME=jialuli3
+echo working directory in $(pwd)/${script}
+echo submitting job $repeat times
+for i in $(seq $repeat); do
+    echo "waiting for $pid to finish ..."
+    # check_squeue () {
+    #     echo "$(squeue | grep ${USERNAME} | grep ${pid} | xargs)"
+    #}
+    r=$(squeue 2>&1)
+    while true
+    do
+        if [ $? -ne 0 ] || [[ $r == *"slurm_load_jobs error"* ]]; then
+            sleep 30
+        else
+            filtered=$(echo $r | grep ${USERNAME} | grep ${pid} | xargs )
+            if [ ! -z "${filtered}" ]; then
+                sleep 30
+            else
+                break
+
+            fi
+        fi
+        r=$(squeue 2>&1)
+    done
+    echo "submitting job ..."
+    pid=$(swbatch $(pwd)/${script} | cut -d ' ' -f4)
+
+done
